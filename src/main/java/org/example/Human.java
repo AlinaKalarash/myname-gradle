55ef366604b412e8cb20259764065009d3b66ae0
Alina Kalarash <bloha7895@gmail.com>
2024-01-24 21:39:38 +0200

    it works now

--- src/main/java/org/example/Human.java
+++ src/main/java/org/example/Human.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Human {
        public String name;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
