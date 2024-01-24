6f84ea9ce7cba50d5a0f15b3d9b1eddb641b5462
Alina Kalarash <bloha7895@gmail.com>
2024-01-24 18:25:44 +0200

    problem: "Execution failed for task ':Main.main()'.           > Process 'command 'C:\Program Files\Java\jdk-17\bin\java.exe'' finished with non-zero exit value 1"           "Deprecated Gradle features were used in this build, making it incompatible with Gradle 9.0."

---
+++ src/main/java/org/example/Human.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Human {
+        String name;
+        String lastName;
+
+    public Human(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
